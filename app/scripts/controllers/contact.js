aa82e78064e7abdd2324cb492d7399e559c9cf37
Matt Dailey <matthew.dailey1@gmail.com>
2015-09-12 19:11:44 -0400

    Added contact view and scaffolded contact controller

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hashFeedApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the hashFeedApp
+ */
+angular.module('hashFeedApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
