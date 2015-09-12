1dad6fc466e655de6387f0becd5812deb103afd7
Matt Dailey <matthew.dailey1@gmail.com>
2015-09-12 15:12:22 -0400

    Initial commit after yeoman scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackFeedApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hackFeedApp
+ */
+angular.module('hackFeedApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
